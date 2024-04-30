467bf48ad3c0508acf27b4ca744441257ff60453
thomas <bezille.thomas@proton.me>
2024-04-30 11:17:24 +0200

    création de la base du reducer

---
+++ src/reducers/chatReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  // Ici le state initial
+};
+
+const chatReducer = (state = initialState, action = {}) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default reducer;
