25c2608e2d5f0e569c8ae53a71e5d84479f374e4
thomas <bezille.thomas@proton.me>
2024-05-03 10:14:46 +0200

    réécriture du middleware avec des switch

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -2,23 +2,27 @@ import axios from 'axios';
 import { SUBMIT_LOGIN, handleSuccessFulLogin } from '../actions/chat';
 
 const authMiddleware = (store) => (next) => (action) => {
-  if (action.type === SUBMIT_LOGIN) {
-    axios
-      .post(
-        // URL
-        'http://localhost:3001/login',
-        // Les données à envoyer
-        {
-          email: store.getState().email,
-          password: store.getState().password,
-        }
-      )
-      .then((response) => {
-        store.dispatch(handleSuccessFulLogin(response.data.pseudo));
-      })
-      .catch((error) => {
-        console.log('error', error);
-      });
+  switch (action.type) {
+    case SUBMIT_LOGIN:
+      axios
+        .post(
+          // URL
+          'http://localhost:3001/login',
+          // Les données à envoyer
+          {
+            email: store.getState().email,
+            password: store.getState().password,
+          }
+        )
+        .then((response) => {
+          store.dispatch(handleSuccessFulLogin(response.data.pseudo));
+        })
+        .catch((error) => {
+          console.log('error', error);
+        });
+      break;
+
+    default:
   }
 
   next(action);
