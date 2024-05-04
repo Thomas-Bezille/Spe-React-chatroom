b6a63eef58eef6f488ac8c1224f3c8d155c5a96e
thomas <bezille.thomasb@gmail.com>
2024-05-04 10:43:50 +0200

    scroll automatique

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -1,4 +1,5 @@
 import { useSelector } from 'react-redux';
+import { useEffect, useRef } from 'react';
 
 import Message from './Message';
 
@@ -10,8 +11,19 @@ import './Messages.scss';
 const Messages = () => {
   const messages = useSelector((state) => state.messages);
 
+  const refContainer = useRef(null);
+
+  useEffect(() => {
+    const element = refContainer.current;
+    element.scrollTo({
+      left: 0,
+      top: element.scrollHeight,
+      behavior: 'smooth',
+    });
+  }, [messages]);
+
   return (
-    <div className="messages">
+    <div className="messages" ref={refContainer}>
       {messages.map((currentMessage) => (
         <Message key={currentMessage.id} {...currentMessage} />
       ))}
