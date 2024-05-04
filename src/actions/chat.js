ad9a2bf2bedf44ced99442912d490fcdaf06b9ef
thomas <bezille.thomasb@gmail.com>
2024-05-04 10:09:51 +0200

    correctif

--- src/actions/chat.js
+++ src/actions/chat.js
@@ -1,5 +1,4 @@
 export const CHANGE_INPUT_MESSAGE = 'CHANGE_INPUT_MESSAGE';
-export const ADD_MESSAGE = 'ADD_MESSAGE';
 export const TOGGLE_SETTINGS_OPEN = 'TOGGLE_SETTINGS_OPEN';
 export const CHANGE_SETTINGS_FIELD = 'CHANGE_SETTINGS_FIELD';
 export const SUBMIT_LOGIN = 'SUBMIT_LOGIN';
@@ -12,10 +11,6 @@ export const HANDLE_MESSAGE_RECEIVED = 'HANDLE_MESSAGE_RECEIVED';
 export const changeInputMessage = (newValue) => ({
   type: CHANGE_INPUT_MESSAGE,
   value: newValue,
-});
-
-export const addMessage = () => ({
-  type: ADD_MESSAGE,
 });
 
 export const toggleSettingsOpen = () => ({
--- src/components/Messages/Message.jsx
+++ src/components/Messages/Message.jsx
@@ -3,18 +3,18 @@ import PropTypes from 'prop-types';
 /**
  * Affiche un message
  */
-const Message = ({ author, text }) => {
+const Message = ({ author, content }) => {
   return (
     <div className="message">
       <div className="message-author">{author}</div>
-      <div className="message-content">{text}</div>
+      <div className="message-content">{content}</div>
     </div>
   );
 };
 
 Message.propTypes = {
   author: PropTypes.string.isRequired,
-  text: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
 };
 
 export default Message;
